ce4fa6c1f611b74ff68541f329c6b44569a3aeac
John Owusu Duah <67676957+johnowusuduah@users.noreply.github.com>
2021-09-24 20:14:35 -0400

    Delete app directory

--- app/api/v1/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from .endpoints import services
-
-router = APIRouter()
-
-
-router.include_router(services.router, prefix="/test", tags=["Endpoint Test"])
--- app/api/v1/endpoints/services.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-
-router = APIRouter()
-
-
-@router.get("/")
-async def testing_child_resource():
-    return {"message": "Hi There! This is my route endpoint."}
