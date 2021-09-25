9099b1c69ef9c3c9e681e2d37c8471fb1281473d
John Owusu Duah <67676957+johnowusuduah@users.noreply.github.com>
2021-09-24 20:23:08 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import uvicorn
+import datetime
+import calendar
+import string
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome, this function finds the the day of the week of any date in the format - DD MM YYYY"}
+
+@app.get("/finddayofweek/{date}")
+async def finddayofweek(date):
+ 
+
+def findDay(date):
+    born = datetime.datetime.strptime(date, '%d %m %Y').weekday()
+    return (calendar.day_name[born])
+
+  
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
