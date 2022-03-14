8afc11978a6fed5e5dc955937f9baee29ca87947
johnowusuduah <johnoduah@yahoo.co.uk>
2022-03-13 20:28:05 -0400

    debug requirements

--- data/__init__.py
+++ data/__init__.py
@@ -3,7 +3,6 @@ import os
 class Models():
     def __init__(self, market):
         self.from, self.market = None, None
-        self.stack = []
 
     def order(self, do):
         self.from = 2 + 4
