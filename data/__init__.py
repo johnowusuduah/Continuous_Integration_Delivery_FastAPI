ccd542f43f510c0517975302619cc2f1a12235a3
johnowusuduah <johnoduah@yahoo.co.uk>
2022-11-16 02:05:45 -0500

    update Model class

--- data/__init__.py
+++ data/__init__.py
@@ -10,7 +10,7 @@ import datetime
 class Models():
     def __init__(self, market):
         self.from, self.market = None, None
-        self.stack = 
+        self.stack = [None]
 
     def action(self, who):
        return forevers
